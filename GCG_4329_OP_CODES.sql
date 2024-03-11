82a36127a2246a6eac62818d9e13402cf97911a9
Service Web <sw@gssmail.com>
2024-03-11 14:47:37 -0500

    Commit changes in C:\Websites\Git\emarlow\4329\GCG_4329_OP_CODES.sql

---
+++ GCG_4329_OP_CODES.sql
@@ -0,0 +1,9 @@
+/* ==========================================
+ * TABLE: GCG_4329_OP_CODES
+ * ========================================== */
+CREATE TABLE "GCG_4329_OP_CODES"(
+ "ID" IDENTITY DEFAULT '0',
+ "OP_CODE" CHAR(6),
+ UNIQUE ("ID"));
+
+
